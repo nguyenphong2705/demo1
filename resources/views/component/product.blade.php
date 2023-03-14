a879aa89b00801f3bc22cd0e41b6440ae1e279f2
phong2750 <nguyenthanhphong27052000@gmail.com>
2023-03-15 05:34:07 +0700

    create product component

---
+++ resources/views/component/product.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    {{ __('Product') }}
+                </div>
+
+                <div class="card-body">
+                    {{ __('Product') }}
+                </div>
+
+                <div class="card-footer">
+                    {{ __('Product') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
